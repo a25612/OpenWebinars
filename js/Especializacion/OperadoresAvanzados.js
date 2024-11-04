a4056f2ada29e4b2003964047ef0bb7347328f3b
a25612 <a25612@svalero.com>
2024-11-04 13:08:37 +0100

    Code Katas del Curso Especializacion.js

---
+++ js/Especializacion/OperadoresAvanzados.js
@@ -0,0 +1,27 @@
+let myText = "Un texto cualquiera";
+
+console.log(myText + ". " + "Con un final cualquiera");
+
+
+
+
+
+
+
+/*let exponent = 3;
+let numberSample = 7;
+
+console.log(numberSample ** exponent);*/
+
+
+/*let num1 = 18;
+let num2 = 33;
+
+let result = num1 % 2;
+
+if (result == 0) {
+    console.log("Es par");
+} else if (result!= 0) {
+    console.log("Es impar");
+}
+*/
---
+++ js/Especializacion/Tipos_no_primitivos.js
@@ -0,0 +1,32 @@
+let currentDate = new Date();
+
+console.log(currentDate);
+
+let secondcurrentDate = new Date();
+ 
+console.log(secondcurrentDate);
+
+
+
+/*let objeto = {
+    name: "Ruben",
+    surname: "Arnadillo"
+};
+
+console.log(objeto);*/
+
+
+
+/*let emunu = new Set([1, 7, true, "Ruben"]);
+
+emunu.add("Adios");
+
+console.log(emunu);*/
+
+
+
+/*let nameCollection = ["Armando", "Ruben", "David", "Plo"];
+
+console.log(nameCollection);
+
+console.log(nameCollection[3]);*/
